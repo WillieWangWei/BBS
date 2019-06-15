5dfa383e65ca17cd45909d9498c0116a92d7488d
Willie <willie.wangwei@gmail.com>
2019-06-15 17:35:51 +0800

    Seed users and topics

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -1,9 +1,22 @@
 <?php
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+
+    // 随机取一个月以内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+
+    // 传参为生成最大时间不超过，因为创建时间需永远比更改时间要早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'excerpt' => $sentence,
+        'body' => $faker->text(),
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(Faker\Generator::class);
+
+        // 头像假数据
+        $avatars = [
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png',
+        ];
+
+        // 生成数据集合
+        $users = factory(User::class)
+            ->times(10)
+            ->make()
+            ->each(function ($user)
+            use ($faker, $avatars)
+            {
+                // 从头像数组中随机取出一个并赋值
+                $user->avatar = $faker->randomElement($avatars);
+            });
+
+        // 让隐藏字段可见，并将数据集合转换为数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入到数据库中
+        User::insert($user_array);
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name   = 'Willie';
+        $user->email  = 'willie.wangwei@gmail.com';
+        $user->avatar = 'https://avatars2.githubusercontent.com/u/25432264?s=460&v=4';
+        $user->save();
+    }
+}
